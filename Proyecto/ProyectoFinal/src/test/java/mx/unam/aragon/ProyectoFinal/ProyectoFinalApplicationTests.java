27af60890bf52eaaeee3409efdbe79e30023b2bc
Tristán Javier Cruz Hernández <124111638+TristanHDZ1@users.noreply.github.com>
2025-06-03 00:53:54 -0600

    Add files via upload

---
+++ Proyecto/ProyectoFinal/src/test/java/mx/unam/aragon/ProyectoFinal/ProyectoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.unam.aragon.ProyectoFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
